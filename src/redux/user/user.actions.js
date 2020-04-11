c6fdfe26ff42dbbab18d36ea82d3a77ffac44027
Ricardo Georgel <rickygeorgel@yahoo.com.br>
2020-04-11 14:42:10 -0300

    fixing typo

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,4 @@
-import { UserActionTypes } from './user.types.js';
+import { UserActionTypes } from './user.types';
 
 export const setCurrentUser = user => ({
   type: UserActionTypes.SET_CURRENT_USER,
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,4 +1,4 @@
-import { UserActionTypes } from './user.types.js';
+import { UserActionTypes } from './user.types';
 
 const INITIAL_STATE = {
   currentUser: null
